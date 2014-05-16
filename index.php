3357cf264df2feb1b59250c88f293ee6450e0496
Arman Khalatyan <arm2arm@gmail.com>
2014-05-16 22:44:30 +0200

    Database fixes

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 \OCP\User::checkLoggedIn();
 \OCP\App::checkAppEnabled('files_git');
 
+$username = OC_User::getUser();
+
+#var_dump($username);exit(0);
 $tpl = new OCP\Template("files_git", "main", "user");
-$tpl->assign('msg', 'Hello World');
+$tpl->assign('msg', "Hello World $username");
 $tpl->printPage();
 
